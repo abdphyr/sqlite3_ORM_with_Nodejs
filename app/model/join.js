76f7e380deecb73e87bb76d083934f402aa36f54
Abdumannon <abdphyr@gmail.com>
2023-02-09 15:05:50 +0500

    Add left join class

--- app/model/join.js
+++ app/model/join.js
@@ -1,27 +1,27 @@
 const { Query } = require('./query');
 
-class Join extends Query {
+class LeftJoin extends Query {
   fields;
   joinTable;
   joinCol;
   col;
-  sql;
-  params;
+
   constructor(table, fields, joinTable, joinCol, col) {
     super(table);
     this.fields = fields;
     this.joinTable = joinTable;
     this.joinCol = joinCol;
     this.col = col;
-    this.sql = `SELECT * FROM ${this.table} JOIN ${this.joinTable} ON ${this.table}.${this.col} = ${this.joinTable}.${this.joinCol}`;
+    this.sql = `SELECT * FROM ${this.table} LEFT JOIN ${this.joinTable} ON ${this.table}.${this.col} = ${this.joinTable}.${this.joinCol}`;
   }
 
   selectFromRight(rightTableColumns) {
     let lsel = this.fields.map(item => `${this.table}.${item}`).join(",");
     let rsel = rightTableColumns.split(",").map(item => `${this.joinTable}.${item}`).join(",");
-    this.changeSelect(`${this.table}.id,${lsel},${rsel}`);
+    this.slct = `${this.table}.id,${lsel},${rsel}`;
+    this.changeSelect();
     return this;
   }
 }
 
-module.exports = { Join };
+module.exports = { LeftJoin };
