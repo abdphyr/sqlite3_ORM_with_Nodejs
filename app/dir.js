d621303f03a448cea772fc9cbb778bd7bcfa4a1d
Abdumannon <abdphyr@gmail.com>
2023-02-10 08:29:28 +0500

    Add PORT and export

--- app/dir.js
+++ app/dir.js
@@ -1,3 +1,9 @@
-module.exports = { root: __dirname, host(path) {
-  return "http://localhost:4000/" + path; 
-} };
+const PORT = process.env.PORT || 4000;
+
+module.exports = {
+  root: __dirname,
+  PORT,
+  host(path) {
+    return "http://localhost:" + PORT + "/" + path;
+  }
+};
