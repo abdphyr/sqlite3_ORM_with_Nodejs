5148f61e41fce5321fdab1006e5aeabd1ce9067f
Abdumannon <abdphyr@gmail.com>
2023-02-10 08:30:38 +0500

    Add some changes

--- app/model/all.js
+++ app/model/all.js
@@ -3,8 +3,7 @@ const { Query } = require('./query');
 
 class All extends Query {
   constructor(table) {
-    super(table)
-    this.sql = `SELECT * FROM ${this.table}`;
+    super(table);
   }
 }
 
--- app/model/join.js
+++ app/model/join.js
@@ -5,21 +5,18 @@ class LeftJoin extends Query {
   joinTable;
   joinCol;
   col;
-
   constructor(table, fields, joinTable, joinCol, col) {
     super(table);
     this.fields = fields;
     this.joinTable = joinTable;
     this.joinCol = joinCol;
     this.col = col;
-    this.sql = `SELECT * FROM ${this.table} LEFT JOIN ${this.joinTable} ON ${this.table}.${this.col} = ${this.joinTable}.${this.joinCol}`;
+    this.LEFT_JOIN = `${this.joinTable} ON ${this.table}.${this.col} = ${this.joinTable}.${this.joinCol}`
   }
-
   selectFromRight(rightTableColumns) {
     let lsel = this.fields.map(item => `${this.table}.${item}`).join(",");
-    let rsel = rightTableColumns.split(",").map(item => `${this.joinTable}.${item}`).join(",");
-    this.slct = `${this.table}.id,${lsel},${rsel}`;
-    this.changeSelect();
+    let rsel = rightTableColumns ? rightTableColumns.split(",").map(item => `${this.joinTable}.${item}`).join(",") : null;
+    this.SELECT = rsel ? `${this.table}.id,${lsel},${rsel}` : `${this.table}.id,${lsel}`;
     return this;
   }
 }
